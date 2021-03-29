d7c3cb57f72376c2675c98c338d88d5311be82fa
ydoi <doi@mmmcorp.co.jp>
2021-03-30 01:03:20 +0900

    [change]umehara bot

--- test.js
+++ test.js
@@ -1,35 +1,18 @@
 function handleRequest(request) {
-  const { pathname } = new URL(request.url);
+  const message = "あまりにも驚いてじゃんがら食いに行っちゃったよ";
+  const html = `<html>
+     <h1>ウメハラ名言Bot</h2>
+     ${message}
+    </html>`;
 
-  // Respond with HTML
-  if (pathname.startsWith("/html")) {
-    const html = `<html>
-      <p><b>Message:</b> Hello from Deno Deploy.</p>
-      </html>`;
-
-    return new Response(html, {
-      headers: {
-        // The "text/html" part implies to the client that the content is HTML
-        // and the "charset=UTF-8" part implies to the client that the content
-        // is encoded using UTF-8.
-        "content-type": "text/html; charset=UTF-8",
-      },
-    });
-  }
-
-  // Respond with JSON
-  if (pathname.startsWith("/json")) {
-    // Use stringify function to convert javascript object to JSON string.
-    const json = JSON.stringify({
-      message: "Hello from Deno Deploy",
-    });
-
-    return new Response(json, {
-      headers: {
-        "content-type": "application/json; charset=UTF-8",
-      },
-    });
-  }
+  return new Response(html, {
+    headers: {
+      // The "text/html" part implies to the client that the content is HTML
+      // and the "charset=UTF-8" part implies to the client that the content
+      // is encoded using UTF-8.
+      "content-type": "text/html; charset=UTF-8",
+    },
+  });
 }
 
 addEventListener("fetch", (event) => {
